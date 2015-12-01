1755ad74345c007b4f8e61748bd91a3fba291cc1
otherness-space <the@otherness.space>
2015-11-30 23:11:48 -0500

    Addition of trivial landmarks to compel new branch

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 if ( ! isset( $content_width ) ) $content_width = 900;
 
@@ -34,7 +34,7 @@ add_action( 'wp_enqueue_scripts', 'materialpress_scripts' );
 function materialpress_nav_menu() {
 	register_nav_menu( 'primary', 'Primary Menu' );
 	register_nav_menu( 'footer', 'Footer Menu' );
-}  
+}
 add_action( 'after_setup_theme', 'materialpress_nav_menu' );
 
 /* Add Waves Effect to Menu Items */
@@ -91,19 +91,22 @@ function materialpress_support() {
 }
 add_action( 'after_setup_theme', 'materialpress_support' );
 
+/** ------------------------------------------------------------------------ */
 /* Customizer API */
 class materialpress_customize {
    public static function register ( $wp_customize ) {
 	  //1. Define a new section (if desired) to the Theme Customizer
-	  $wp_customize->add_section( 'materialpress_options', 
+	  $wp_customize->add_section( 'materialpress_options',
 	     array(
 	        'title' => __( 'MaterialPress Options', 'materialpress' ), //Visible title of section
 	        'priority' => 35, //Determines what order this appears in
 	        'capability' => 'edit_theme_options', //Capability needed to tweak
 	        'description' => __('Customize MaterialPress to your liking.', 'materialpress'), //Descriptive tooltip
-	     ) 
+	     )
 	  );
-	  
+
+/** ------------------------------------------------------------------------ */
+/** ------------------------------------------------------------------------ */
 	  //2. Register new settings to the WP database...
 	  $wp_customize->add_setting( 'link_textcolor', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
 	     array(
@@ -111,9 +114,9 @@ class materialpress_customize {
 	        'type' => 'theme_mod', //Is this an 'option' or a 'theme_mod'?
 	        'capability' => 'edit_theme_options', //Optional. Special permissions for accessing this setting.
 	        'transport' => 'postMessage', //What triggers a refresh of the setting? 'refresh' or 'postMessage' (instant)?
-	     ) 
-	  );      
-	        
+	     )
+	  );
+
 	  //3. Finally, we define the control itself (which links a setting to a section and renders the HTML controls)...
 	  $wp_customize->add_control( new WP_Customize_Color_Control( //Instantiate the color control class
 	     $wp_customize, //Pass the $wp_customize object (required)
@@ -123,9 +126,9 @@ class materialpress_customize {
 	        'section' => 'colors', //ID of the section this control should render in (can be one of yours, or a WordPress default section)
 	        'settings' => 'link_textcolor', //Which setting to load and manipulate (serialized is okay)
 	        'priority' => 10, //Determines the order this control appears in for the specified section
-	     ) 
+	     )
 	  ) );
-	  
+
 	  //4. We can also change built-in settings by modifying properties. For instance, let's make some stuff use live preview JS...
 	  $wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
 	  $wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
@@ -135,22 +138,23 @@ class materialpress_customize {
 
    public static function header_output() {
 	  ?>
-	  <!--Customizer CSS--> 
+	  <!--Customizer CSS-->
 	  <style type="text/css">
-	       <?php self::generate_css('#site-title a', 'color', 'header_textcolor', '#'); ?> 
-	       <?php self::generate_css('body', 'background-color', 'background_color', '#'); ?> 
+	       <?php self::generate_css('#site-title a', 'color', 'header_textcolor', '#'); ?>
+	       <?php self::generate_css('body', 'background-color', 'background_color', '#'); ?>
+/** ------------------------------------------------------------------------ */
 	       <?php self::generate_css('a', 'color', 'link_textcolor'); ?>
-	  </style> 
+	  </style>
 	  <!--/Customizer CSS-->
 	  <?php
    }
-   
+
    public static function live_preview() {
-	  wp_enqueue_script( 
+	  wp_enqueue_script(
 	       'materialpress-themecustomizer', // Give the script a unique ID
 	       get_template_directory_uri() . '/js/theme-customizer.js', // Define the path to the JS file
 	       array(  'jquery', 'customize-preview' ), // Define dependencies
-	       '', // Define a version (optional) 
+	       '', // Define a version (optional)
 	       true // Specify whether to put in footer (leave this true)
 	  );
    }

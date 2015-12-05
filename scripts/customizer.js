0cf51211f8272809cc69532d572b9b8a848de617
otherness-space <the@otherness.space>
2015-12-05 18:57:03 -0500

    jsLint is throwing errors... no eros..

--- scripts/customizer.js
+++ scripts/customizer.js
@@ -1,11 +1,20 @@
-/**
-* Theme Customizer enhancements for a better user experience
-*
-* Contains handlers to make Theme Customizer preview reload changes asynchronously
-*/
-(function ($) {
-
-// Update site background color.
-wp.customize('background_color', function (value) {value.bind(function (newval) { $('body').css('background-color', newval); }); });
+(function($) {
+    wp.customize('background_color',
+        function(value) {
+            value.bind(function(newval) {
+                $('body')
+                    .css('background-color',
+                        newval
+                    );
+            });
+        });
 })
 (jQuery);
+
+/**
+ * Theme Customizer enhancements for a better user experience
+ *
+ * Contains handlers to make Theme Customizer preview reload changes asynchronously
+ */
+
+// Update site background color.

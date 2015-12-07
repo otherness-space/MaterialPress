d7db6552ae8f6db54ec19f485be301d1101d341a
otherness-space <the@otherness.space>
2015-12-07 00:13:23 -0500

    Since I have been denied for hours to ge the text to change colors in the title I will just leave it.

--- functions.php
+++ functions.php
@@ -172,31 +172,6 @@ $wp_customize->add_control( new WP_Customize_Color_Control( //Instantiate the co
 /**--------------------------------------------------------------------------*/
 
 //2. Register new settings to the WP database...
-$wp_customize->add_setting( 'nav_textcolor', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
-	array(
-		'default' => '#000000', //Default setting/value to save
-		'type' => 'theme_mod', //Is this an 'option' or a 'theme_mod'?
-		'capability' => 'edit_theme_options', //Optional. Special permissions for accessing this setting.
-		'transport' => 'postMessage', //What triggers a refresh of the setting? 'refresh' or 'postMessage' (instant)?
-		'sanitize_callback' => 'sanitize_hex_color',
-	)
-);
-
-//3. Finally, we define the control itself (which links a setting to a section and renders the HTML controls)...
-$wp_customize->add_control( new WP_Customize_Color_Control( //Instantiate the color control class
-	$wp_customize, //Pass the $wp_customize object (required)
-		'materialpress_nav_textcolor', //Set a unique ID for the control
-			array(
-				'label' => __( 'Nav Text Color', 'materialpress' ), //Admin-visible name of the control
-				'section' => 'colors', //ID of the section this control should render in (can be one of yours, or a WordPress default section)
-				'settings' => 'nav_textcolor', //Which setting to load and manipulate (serialized is okay)
-				'priority' => 104, //Determines the order this control appears in for the specified section
-			)
-) );
-
-/**--------------------------------------------------------------------------*/
-
-//2. Register new settings to the WP database...
 $wp_customize->add_setting( 'html_textcolor', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
 	array(
 		'default' => '#000000', //Default setting/value to save
@@ -470,12 +445,6 @@ $wp_customize->add_control( new WP_Customize_Color_Control( //Instantiate the co
 	self::generate_css('nav',
 		'background-color',
 		'nav_background_color'
-	);
-	self::generate_css('nav .brand-logo',
-		'nav a',
-		'nav',
-		'color',
-		'nav_textcolor'
 	);
 	self::generate_css('html',
 		'color',
